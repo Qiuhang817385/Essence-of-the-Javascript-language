3649502ca46b0de62512907f34cb6761bfb5f292
Qiuhang817385 <826266129@qq.com>
2020-06-23 23:04:22 +0800

    stage1

---
+++ vue-datav/src/plugins/element.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import {
+  Card,
+  Col,
+  Row,
+  DatePicker,
+  RadioButton,
+  RadioGroup,
+  Menu,
+  MenuItem,
+  Table,
+  TableColumn,
+  Pagination,
+} from 'element-ui'
+
+Vue.use(Card)
+Vue.use(Col)
+Vue.use(Row)
+Vue.use(DatePicker)
+Vue.use(RadioButton)
+Vue.use(RadioGroup)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Pagination)
